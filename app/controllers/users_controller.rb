38205f200b8f06c5a5101fa1c37ea6a756f5150d
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-10-03 14:15:42 -0400

    user serializers created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,16 @@ class UsersController < ApplicationController
 
     def index
         users = User.all
+        render json: users, except: [:updated_at, :created_at]
     end
 
     def show
         user = User.find(params[:id])
+        render json: user, except: [:updated_at, :created_at]
     end
 
 private
 
-def users_params
+def user_params
     params.require(:user).permit(:name, :username, :password_digest)
 end
