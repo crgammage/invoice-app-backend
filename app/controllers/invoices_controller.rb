abca9b4aeec04db4e516d6ef10d8dbb718e8e489
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-10-03 14:15:05 -0400

    invoices controller serializers created

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,10 +2,12 @@ class InvoicesController < ApplicationController
 
     def index
         invoices = Invoice.all
+        render json: invoices, except: [:updated_at, :created_at]
     end
 
     def show
         invoice = Invoice.find(params[:id])
+        render json: invoice, except: [:updated_at, :created_at]
     end
 
     private
