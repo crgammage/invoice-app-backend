53167f43f14c13b08e022df43d6a79806d7ffca3
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-10-03 14:14:21 -0400

    serializers created for clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,15 +2,17 @@ class ClientsController < ApplicationController
 
     def index
         clients = Client.all
+        render json: clients, except: [:updated_at, :created_at]
     end
 
     def show
         client = Client.find(params[:id])
+        render json: client, except: [:updated_at, :created_at]
     end
 
     private
 
-    def clients_params
+    def client_params
         params.rquire(:client).permit(:name, :address)
     end
 end
