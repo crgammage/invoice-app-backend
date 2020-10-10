a9c1e98b11a6eccd947edf7e945c5ffaa3f677d7
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-10-10 12:32:56 -0400

    json serializers created

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,10 +2,12 @@ class ClientsController < ApplicationController
 
     def index
         clients = Client.all
+        render json: clients, except: [:updated_at, :created_at]
     end
 
     def show
         client = Client.find(params[:id])
+        render json: client, except: [:updated_at, :created_at]
     end
 
     private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
 
     def index
         users = User.all
+        render json: users, include: [:clients], except: [:updated_at, :createda_at]
     end
 
     def show
         user = User.find(params[:id])
+        render json: user, except: [:updated_at, :createda_at]
     end
 
     def create
